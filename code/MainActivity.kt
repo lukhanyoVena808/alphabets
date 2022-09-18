ad2c8ff2ccd03e458cf08b5c9edb55eef0959e63
lukhanyoVena808 <lookmane001@gmail.com>
2022-09-18 19:58:41 +0200

    version 8.0

--- code/MainActivity.kt
+++ code/MainActivity.kt
@@ -16,89 +16,105 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/*
+* MainActivity contains the buttons to all letters including their listeners.
+* When activity starts, the saved state is loaded, if empty start the app as usual
+* else navigate to letter page to view the saved state
+* */
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
 
-    private var display: String? = ""
-    private var strIntent: String? = null
-    private var buttonText: String? = ""
-    private var theIntent: Intent? = null
+
+    private var strIntent: String? = null  //holds  an intent uri (address)
+    private lateinit var buttonText: String   //text of button/ current Image
+    private var theIntent: Intent? = null //intent sent to letters' page
 
 
-    private lateinit var myBar: ProgressBar
+    private lateinit var myBar: ProgressBar  // activity bar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        loadData()
-            val alphabets = arrayListOf(
-                R.id.buttonA, R.id.buttonB, R.id.buttonC,
-                R.id.buttonD, R.id.buttonE, R.id.buttonF, R.id.buttonG, R.id.buttonH, R.id.buttonI,
-                R.id.buttonJ, R.id.buttonK, R.id.buttonL,
-                R.id.buttonM, R.id.buttonN, R.id.buttonO,
-                R.id.buttonP, R.id.buttonQ, R.id.buttonR, R.id.buttonS,
-                R.id.buttonT, R.id.buttonU, R.id.buttonV, R.id.buttonW,
-                R.id.buttonX, R.id.buttonY, R.id.buttonZ
-            )
-            myBar = findViewById(R.id.progressBar1)
+        buttonText = getString(R.string.empty)
+        loadData()  //load saved state
+        val alphabets = arrayListOf( //keeps id's of all buttons
+            R.id.buttonA, R.id.buttonB, R.id.buttonC,
+            R.id.buttonD, R.id.buttonE, R.id.buttonF, R.id.buttonG, R.id.buttonH, R.id.buttonI,
+            R.id.buttonJ, R.id.buttonK, R.id.buttonL,
+            R.id.buttonM, R.id.buttonN, R.id.buttonO,
+            R.id.buttonP, R.id.buttonQ, R.id.buttonR, R.id.buttonS,
+            R.id.buttonT, R.id.buttonU, R.id.buttonV, R.id.buttonW,
+            R.id.buttonX, R.id.buttonY, R.id.buttonZ
+        )
+        myBar = findViewById(R.id.progressBar1)  //initialize activity bar
 
-            for (alpha in alphabets) {
-                val btn = findViewById<Button>(alpha)
-                btn.setOnClickListener {
-                    buttonText = btn.text.toString()
-                    theIntent = Intent(this, MainActivity2::class.java)
-                    theIntent!!.flags = Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-                    strIntent = theIntent!!.toUri(0)
-                    val delayed = DelayTask()
-                    delayed.execute()
-                }
+        //for each button, create a listener, if the button is clicked, its text which corresponds
+        //to the image name is sent to the letter page to be viewed
+        for (alpha in alphabets) {
+            val btn = findViewById<Button>(alpha)
+            btn.setOnClickListener {
+                buttonText = btn.text.toString()  //gets the text of the button (image name)
+                theIntent = Intent(this, MainActivity2::class.java)
+                strIntent = theIntent!!.toUri(0)
+                val delayed = DelayTask()  //to activate activity bar and send intent using threads
+                delayed.execute()
+
             }
+        }
 
-        if (display != null && display!!.isNotEmpty()) {
-            Toast.makeText(this@MainActivity, "Loading Previous Image", Toast.LENGTH_SHORT+100).show()
-            SystemClock.sleep(10)
+        //After the loading data, check if there is a current state saved and display the image
+        // else start the app as normal
+        // *** null is not a string and cannot be saved to strings.xml
+        if (buttonText != "null" && buttonText != getString(R.string.empty)) {
+            Toast.makeText(this@MainActivity, getString(R.string.loading), Toast.LENGTH_SHORT).show()
+            SystemClock.sleep(25)  //creates a delay before sending current state to letter page
             val newIntent = Intent(this, MainActivity2::class.java)
-            newIntent.flags = Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
-            newIntent.putExtra(getString(R.string.intent1), display)
-            startActivity(newIntent)
-            buttonText =null
-            display = null
+            newIntent.putExtra(getString(R.string.intent1), buttonText)
+
+            //clears the saved state, *** null is not a string and cannot be saved to strings.xml
+            buttonText = "null"
+            saveData()  //upDates the save state to null
+            startActivity(newIntent) //send current state
+
         }
         else {
             opening()
-            val nIntent: String = getString(R.string.intent2)
-            buttonText = intent.getStringExtra(nIntent)
+
         }
 
     }
 
+    //message to user, when starting the app afresh
     private fun opening(){
         Toast.makeText(this@MainActivity, getString(R.string.opening), Toast.LENGTH_SHORT).show()
 
     }
 
-@SuppressLint("StaticFieldLeak")
-open inner class DelayTask : AsyncTask<Void, Any, Any>() {
+    @SuppressLint("StaticFieldLeak")
+    //inner class to create a delay before sending the intent to letter page using threading
+    open inner class DelayTask : AsyncTask<Void, Any, Any>() {
 
         var count = 0
 
         @Deprecated("Deprecated in Java")
         override fun onPreExecute() {
-            myBar.visibility = View.VISIBLE
+            myBar.visibility = View.VISIBLE   //makes the activity bar visible
         }
 
 
         @Deprecated("Deprecated in Java")
         @Suppress("DEPRECATION")
+        // creates a delay
         override fun doInBackground(vararg params: Void?) {
             while (count < 1) {
                 SystemClock.sleep(500)
                 count++
                 publishProgress(count * 20)
             }
+
+            //send intent to letter page
             val newIntent = Intent.getIntent(strIntent)
-            newIntent.flags = Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS
             newIntent!!.putExtra(getString(R.string.intent1), buttonText)
             startActivity(newIntent)
 
@@ -109,45 +125,33 @@ open inner class DelayTask : AsyncTask<Void, Any, Any>() {
             myBar.progress = values[0] as Int
         }
 
-    @Deprecated("Deprecated in Java")
-    override fun onPostExecute(result: Any?) {
-        myBar.visibility = View.GONE
+        @Deprecated("Deprecated in Java")
+        override fun onPostExecute(result: Any?) {
+            myBar.visibility = View.GONE   //removes activity bar after execution
+        }
     }
-}
 
     @SuppressLint("CommitPrefEdits")
+    //saves the current state of the app, by saving the buttonText which contains the current image
     fun saveData() {
-        val sharedPreferences : SharedPreferences = getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
+        val sharedPreferences : SharedPreferences = getSharedPreferences(getString(R.string.password), Context.MODE_PRIVATE)
         val editor : SharedPreferences.Editor = sharedPreferences.edit()
         editor.apply{
-            putString("button808", buttonText)
+            putString(getString(R.string.key), buttonText)
         }.apply()
 
     }
 
 
+    //loads the current state of app by retrieving the last stored image
     private fun loadData() {
         val sharedPreferences: SharedPreferences =
-            getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
-        display = sharedPreferences.getString("button808",null)
+            getSharedPreferences(getString(R.string.password), Context.MODE_PRIVATE)
+        buttonText = sharedPreferences.getString(getString(R.string.key),null).toString()
 
     }
 
-//    override fun onPause(){
-//        super.onPause()
-////        if(checker){
-////            saveData2()
-////        }
-////        else{
-//            saveData()
-////        }
-//
-//    }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        saveData()
-        Toast.makeText(this@MainActivity,"App is Destroyed", Toast.LENGTH_SHORT).show()
-    }
+
 
 }
--- code/MainActivity2.kt
+++ code/MainActivity2.kt
@@ -3,7 +3,6 @@
 package com.example.my2002s
 import android.annotation.SuppressLint
 import android.content.Context
-import android.content.Intent
 import android.content.SharedPreferences
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
@@ -18,142 +17,163 @@ import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
+/*
+* MainActivity2 contains 5 buttons (Overview, Previous, Next, Last and First) including their listeners.
+* An intent is received from the MainActivity and is display. A user can then get the previous letter,
+* next letter, last or fast letter of the alphabet.
+* The Overview(Top Left of screen) allows the user the navigate back to MainActivity
+* the current state if the is only saved when the user exits the app without navigating back
+* to the home app...in other words if the app is destroyed while an image is bring viewed
+*
+*  --- Added:  android:parentActivityName=".MainActivity" in under the MainActivity2 activity in
+*               the AndroidManifest.xml file to enable the actionbar (Overview Button) to navigate
+*               to the Overview page when pressed.
+* */
 
 @Suppress("DEPRECATION")
 class MainActivity2 : AppCompatActivity(){
 
 
-    private var nextImg : String? = null
-    private var slide ="@string/empty"
-    private var currentPosition: Int = 0   //local variable
-    private lateinit var imgViewNew: ImageView
-    private lateinit var theBar: ProgressBar
+    private var nextImg : String? = null  //contains the the name of image to be viewed
+    private lateinit var slide: String  //string slicing variable
+    private var currentPosition: Int = 0   //local variable //retains current position of image
+    private lateinit var imgViewNew: ImageView //the imageView to display teh images
+    private lateinit var theBar: ProgressBar  // the activity bar
+    private var DestroyApp = true  //boolean to check if is destroyed
 
     @SuppressLint("RestrictedApi")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        val sharedPreferences: SharedPreferences =
-            getSharedPreferences("sharedPrefs", Context.MODE_PRIVATE)
-        val display = sharedPreferences.getString("button808",null)
-        Toast.makeText(this@MainActivity2, "$display", Toast.LENGTH_SHORT).show()
 
-        theBar = findViewById(R.id.bar2)
-        val actionBar = supportActionBar
-        actionBar!!.setDefaultDisplayHomeAsUpEnabled(true)  //activate back button
-        val nIntent: String = getString(R.string.intent1)
-        val alpha = intent.getStringExtra(nIntent)
-        imgViewNew = findViewById(R.id.imageView1)
-        slide = getString(R.string.slider)
+        theBar = findViewById(R.id.bar2)  //initialize activity bar
+        val actionBar = supportActionBar //initialize Overview button
 
-        displayOnThread(alpha, imgViewNew)
+        actionBar!!.setDefaultDisplayHomeAsUpEnabled(true)  //activates overview to return to
+        val nIntent: String = getString(R.string.intent1)  //has name of intent to be retrieved
+        val alpha = intent.getStringExtra(nIntent)  //get the value of the intent
+        imgViewNew = findViewById(R.id.imageView1)  //initialize the imageView
+        slide = getString(R.string.slider)  //initialize string slicer variable
+        nextImg = alpha  //update current image name with the intent value
+        displayOnThread(alpha, imgViewNew) //display image from intent
+
+        //get current position of image
         currentPosition = (alpha?.substring(slide.length, alpha.length))!!.toInt()
 
-        previous(imgViewNew)  //displays previous image
-        next(imgViewNew)  //next image
-        last() //last image
-        first() //first image
+        //button listener for previous button
+        previous(imgViewNew)
+
+        //button listener for next button
+        next(imgViewNew)
+
+        //button listener for 'last' button
+        last()
+
+        //button listener for 'first' button
+        first()
     }
 
+    // Enabled to actionbar (Overview button to navigate to the home page
     override fun onContextItemSelected(item: MenuItem): Boolean {
-            when (item.itemId){
-                android.R.id.home -> {
-                    finish()
-                    return true
-                }
+        when (item.itemId){
+            android.R.id.home -> {
+                finish()
+                return true
             }
+        }
         return super.onContextItemSelected(item)
     }
 
-    /*
-    * Get Id on String
-    * */
+
     @SuppressLint("DiscouragedApi")
+    //Returns the id of the image given its name
     private fun getImage(imageName: String?): Int {
         val draw = getString(R.string.draw)
         return resources.getIdentifier(imageName, draw,packageName)
     }
 
-    // Get previous letter
+    //When the previous button is pressed, the function checks if the current image is not
+    //the first image if not then decrement, else start form the last letter
     private fun previous(imgView: ImageView){
         val btnPrevious = findViewById<Button>(R.id.button_p)
         btnPrevious.setOnClickListener{
 
             if (currentPosition !=1){
                 currentPosition--
-                nextImg = slide+"$currentPosition"
-                displayOnThread(nextImg, imgView)
+                nextImg = slide+"$currentPosition"  //update name of image being viewed
+                displayOnThread(nextImg, imgView)  //display image
             }
             else{
                 currentPosition=26
-                val logic = Logic()
+                val logic = Logic()   //show activity bar while going to last image
                 logic.execute()
 
             }
-//            myIntent()
         }
     }
 
-    // * Get next letter
+    //When the next button is pressed, the function checks if the current image is not
+    //the last image if not then increment, else start form the first letter
     private fun next(imgView: ImageView){
-        //get next letter
+
         val btnNext = findViewById<Button>(R.id.button_n)
         btnNext.setOnClickListener{
             if (currentPosition !=26){
                 currentPosition++
-                nextImg = slide+"$currentPosition"
-                displayOnThread(nextImg, imgView)
+                nextImg = slide+"$currentPosition" //update name of image being viewed
+                displayOnThread(nextImg, imgView) //display image
             }
             else{
                 currentPosition=1
-                val logic = Logic()
+                val logic = Logic() //activate activity while loading to last image
                 logic.execute()
             }
-//            myIntent()
         }
     }
 
-    //get last letter
+    //When the 'last' button is pressed, the function checks if the current image is not
+    //the last image if not navigate and display image, else send message to user
     private fun last() {
         val btnNext = findViewById<Button>(R.id.button_last)
         btnNext.setOnClickListener{
             if (currentPosition ==26){
-                    onLastPage()
+                onLastPage()  //message to tell the user this the last letter
             }
             else{
                 currentPosition=26
-                val logic = Logic()
+                val logic = Logic() //loads activity and displays image
                 logic.execute()
             }
-//            myIntent()
         }
     }
 
-    //get first letter
+    //When the 'first' button is pressed, the function checks if the current image is not
+    //the first image if not navigate to and display image, else send message to user
     private fun first(){
         val btnNext = findViewById<Button>(R.id.button_first)
         btnNext.setOnClickListener{
             if (currentPosition ==1){
-                onFirstPage()
+                onFirstPage() //message to tell the user this the last letter
             }
             else{
                 currentPosition=1
-                val logic = Logic()
+                val logic = Logic() //loads activity and displays image
                 logic.execute()
             }
-//            myIntent()
         }
     }
 
+    //Message alert sent to user when already on last page
     private fun onLastPage(){
         Toast.makeText(this@MainActivity2, getString(R.string.lastPhrase), Toast.LENGTH_SHORT).show()
     }
 
+    //Message alert sent to user when already on first page
     private fun  onFirstPage(){
         Toast.makeText(this@MainActivity2, getString(R.string.firstPhrase), Toast.LENGTH_SHORT).show()
     }
 
-    //display image using Runnable thread
+    //display image using Runnable thread and scaling image down using BitmapFactory
     private fun displayOnThread(string: String?, imgView: ImageView) {
         Thread {
             this@MainActivity2.runOnUiThread {
@@ -165,49 +185,68 @@ class MainActivity2 : AppCompatActivity(){
 
 
     @SuppressLint("StaticFieldLeak")
+    //Thread activates the activity bar and creates a delay before displaying image
     inner class Logic : AsyncTask<Void, Any, Any>() {
         var count = 0
         @Deprecated("Deprecated in Java")
         override fun onPreExecute() {
-            theBar.visibility = View.VISIBLE
+            theBar.visibility = View.VISIBLE  //enables action bar
         }
 
         @Deprecated("Deprecated in Java")
         @Suppress("DEPRECATION")
+        //creates delay
         override fun doInBackground(vararg params: Void?) {
             while (count < 2) {
                 SystemClock.sleep(300)
                 count++
                 publishProgress(count * 20)
             }
-            nextImg = slide+"$currentPosition"
+            nextImg = slide+"$currentPosition" //updates name of image viewed
+
+            //display scaled image using
             val bm = BitmapFactory.decodeResource(resources, getImage(nextImg))
             imgViewNew.setImageBitmap(Bitmap.createScaledBitmap(bm, 500, 500, false))
         }
 
         @Deprecated("Deprecated in Java")
+        //increments speed of activity bar
         override fun onProgressUpdate(vararg values: Any?) {
             theBar.progress = values[0] as Int
         }
 
         @Deprecated("Deprecated in Java")
         override fun onPostExecute(result: Any?) {
-            theBar.visibility = View.GONE
+            theBar.visibility = View.GONE  //remove activity bar
         }
     }
 
+    //save current state only when the app is about to be destroyed (closing app)
     override fun onPause(){
+        if(DestroyApp){
+            saveData()  //save name of current image
+        }
         super.onPause()
-        val theIntent = Intent(this, MainActivity::class.java)
-        theIntent.putExtra(getString(R.string.intent2), nextImg)
-        startActivity(theIntent)
 
     }
 
+    //saves the current state by saving teh name of the current image, using SharedPreferences
+    private fun saveData() {
+        val sharedPreferences: SharedPreferences = getSharedPreferences(getString(R.string.password),Context.MODE_PRIVATE)
+        val editor : SharedPreferences.Editor = sharedPreferences.edit()
+        editor.apply{
+            putString(getString(R.string.key),nextImg)
+        }.apply()
+    }
 
+
+    //Listener for actionbar (Overview button)
+    //when pressed, make sure current state is not saved by setting the 'DestroyApp
+    // boolean to false
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        Toast.makeText(this@MainActivity2, "Bye-Bye", Toast.LENGTH_SHORT).show()
-        myIntent()
+        //Display Message to user, that the are returning to the Overview Page
+        Toast.makeText(this@MainActivity2, getString(R.string.overview), Toast.LENGTH_SHORT).show()
+        DestroyApp = false  //DestroyApp to False, to prevent saving
         return when (item.itemId) {
             android.R.id.home -> {
                 finish()
@@ -215,18 +254,6 @@ class MainActivity2 : AppCompatActivity(){
             }
             else -> super.onOptionsItemSelected(item)
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        Toast.makeText(this@MainActivity2, "Hey Lux MAN", Toast.LENGTH_SHORT).show()
-
-    }
-
-    private fun myIntent(){
-        val theIntent = Intent(this, MainActivity::class.java)
-        theIntent.putExtra(getString(R.string.intent2), nextImg)
-        startActivity(theIntent)
     }
 
 }
